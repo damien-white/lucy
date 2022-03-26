1fbfa2bf2c9ba2116cc0f948eb45bec25402ea5d
Peter Donovan <peter@darkfusion.io>
2022-03-25 21:32:42 -0400

    Add a few combinators and touch on error handling
    The two parsers added were to begin exploring error handling within nom.
    Currently, no parsers can backtrack or recover from failures. Tests were
    added to prove that all parsers (including the ones that already
    existed) work as intended.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use nom::error::ParseError;
 use nom::{IResult, Parser};
 
 pub fn parse_hello_prefix(input: &str) -> IResult<&str, &str, ()> {
@@ -56,18 +57,73 @@ pub fn parse_separated<I, O1, O2, S, E>(
     }
 }
 
+pub fn parse_bool(input: &str) -> IResult<&str, bool, ()> {
+    match parse_tag("true").parse(input) {
+        Ok((tail, _)) => Ok((tail, true)),
+        // This here throws away useful error information. The branch that
+        // discards the error information contains the `_err` match arm var
+        Err(nom::Err::Error(_err)) => match parse_tag("false").parse(input) {
+            Ok((tail, _)) => Ok((tail, false)),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
+
+pub fn parse_either<I, O, E>(
+    mut first: impl Parser<I, O, E>,
+    mut second: impl Parser<I, O, E>,
+) -> impl Parser<I, O, E>
+where
+    I: Clone,
+    E: ParseError<I>,
+{
+    move |input: I| match first.parse(input.clone()) {
+        Ok((tail, value)) => Ok((tail, value)),
+        Err(nom::Err::Error(err1)) => match second.parse(input) {
+            Ok((tail, value)) => Ok((tail, value)),
+            Err(nom::Err::Error(err2)) => Err(nom::Err::Error(err1.or(err2))),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // N.B. This parser works in a vacuum but contains a significant bug. It throws away any errors!
+    #[test]
+    fn parse_boolean_works() {
+        assert_eq!(parse_bool("true, 0123456789"), Ok((", 0123456789", true)));
+        assert_eq!(parse_bool("false, 0123456789"), Ok((", 0123456789", false)));
+        assert!(parse_bool("0123456789, hello").is_err());
+    }
+
+    // TODO: Add proper error handling as failures are currently not recoverable
+    #[test]
+    fn parse_either_works_properly() {
+        // let err_msg = "error handling not yet implemented";
+        let mut parser = parse_either(parse_tag("true"), parse_tag("false"));
+        assert_eq!(
+            parser.parse("true, 0123456789"),
+            Ok((", 0123456789", "true"))
+        );
+        assert_eq!(
+            parser.parse("false, 0123456789"),
+            Ok((", 0123456789", "false"))
+        );
+        assert!(parser.parse("Hello, World!").is_err());
+        assert!(parser.parse("0123456789, true").is_err());
+
+        assert!(parser.parse("1, 0123456789").is_err());
+    }
+
     #[test]
     fn top_level_parser_works() {
-        let mut parse_hello_world =
-            parse_separated(parse_tag("Hello"), parse_tag(", "), parse_tag("World"));
-        assert_eq!(
-            parse_hello_world.parse("Hello, World!"),
-            Ok(("!", ("Hello", "World")))
-        );
+        let mut parser = parse_separated(parse_tag("Hello"), parse_tag(", "), parse_tag("World"));
+        assert_eq!(parser.parse("Hello, World!"), Ok(("!", ("Hello", "World"))));
     }
 
     #[test]
