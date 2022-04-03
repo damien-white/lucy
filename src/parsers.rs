d84c04c29279bb490a11a81bd579535ae2a1b7d5
Peter Donovan <peter@darkfusion.io>
2022-04-02 22:05:55 -0400

    Improve test quality/coverage and refactor parsers
    This project is being developed using ideas from test-driven
    development. It's important that the test suite (currently just unit
    tests) is easily extensible and can be built upon.
    
    More tests were added with error cases included in order to ensure that
    both common and edge-cases are covered.
    
    Existing parser combinator functions were refactored slightly to
    clean them up, simplify names/naming and make them more consistent
    overall.

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,64 +1,94 @@
 use nom::branch::alt;
+use nom::bytes::complete::escaped;
 use nom::bytes::complete::{is_not, take_while};
-// use nom::error::ParseError;
+use nom::character::complete::{alphanumeric1, one_of};
 use nom::sequence::delimited;
 use nom::{bytes::complete::tag, combinator::value, IResult};
 
-pub fn boolean(input: &[u8]) -> IResult<&[u8], bool> {
-    alt((value(true, tag("true")), value(false, tag("false"))))(input)
+pub fn boolean(i: &[u8]) -> IResult<&[u8], bool> {
+    let parse_true = value(true, tag("true"));
+    let parse_false = value(false, tag("false"));
+
+    alt((parse_true, parse_false))(i)
 }
 
-pub fn nullish(input: &[u8]) -> IResult<&[u8], ()> {
-    value((), tag("null"))(input)
+pub fn nullish(i: &[u8]) -> IResult<&[u8], ()> {
+    value((), tag("null"))(i)
 }
 
-pub fn object(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    delimited(tag("{"), is_not("}"), tag("}"))(input)
+pub fn string(i: &[u8]) -> IResult<&[u8], &[u8]> {
+    escaped(alphanumeric1, '\\', one_of("\"n\\"))(i)
 }
 
-pub fn whitespace(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    let tokens = " \t\r\n";
-    take_while(move |token| tokens.contains(token as char))(input)
+pub fn object(i: &[u8]) -> IResult<&[u8], &[u8]> {
+    delimited(tag("{"), is_not("}"), tag("}"))(i)
+}
+
+pub fn whitespace(i: &[u8]) -> IResult<&[u8], &[u8]> {
+    let tokens = [b' ', b'\t', b'\r', b'\n'].as_slice();
+
+    take_while(move |t: u8| tokens.contains(&t))(i)
 }
 
 #[cfg(test)]
 mod tests {
-    use nom::{
-        error::{Error, ErrorKind},
-        Err,
-    };
+    use nom::error::{Error, ErrorKind};
 
     use super::*;
 
     #[test]
-    fn parse_boolean_works() {
+    fn boolean_values() {
         assert_eq!(boolean(&b"true\"\nxxx"[..]), Ok((&b"\"\nxxx"[..], true)));
-        assert_eq!(
-            boolean(&b"asdf"[..]),
-            Err(nom::Err::Error(nom::error::Error::new(
-                &b"asdf"[..],
-                nom::error::ErrorKind::Tag,
-            )))
-        );
         assert_eq!(boolean(&b"falsexyz"[..]), Ok((&b"xyz"[..], false)));
-
+        assert_eq!(
+            boolean(&b"abcdef"[..]),
+            Err(nom::Err::Error(Error::new(&b"abcdef"[..], ErrorKind::Tag)))
+        );
         assert_eq!(
             boolean(&b"xyzfalse"[..]),
-            Err(Err::Error(Error::new(&b"xyzfalse"[..], ErrorKind::Tag)))
-        );
-        assert_eq!(
-            boolean(&b"null"[..]),
-            Err(nom::Err::Error(nom::error::Error::new(
-                &b"null"[..],
-                nom::error::ErrorKind::Tag,
+            Err(nom::Err::Error(Error::new(
+                &b"xyzfalse"[..],
+                ErrorKind::Tag,
             )))
         );
         assert_eq!(
             boolean(&b""[..]),
-            Err(nom::Err::Error(nom::error::Error::new(
-                &b""[..],
-                nom::error::ErrorKind::Tag,
+            Err(nom::Err::Error(Error::new(&b""[..], ErrorKind::Tag)))
+        );
+    }
+
+    #[test]
+    fn nullish_values() {
+        assert_eq!(nullish(&b"nullabcd"[..]), Ok((&b"abcd"[..], ())));
+        assert_eq!(
+            nullish(&b"abcdef"[..]),
+            Err(nom::Err::Error(Error::new(&b"abcdef"[..], ErrorKind::Tag)))
+        );
+        assert_eq!(
+            nullish(&b"abcdnull"[..]),
+            Err(nom::Err::Error(Error::new(
+                &b"abcdnull"[..],
+                ErrorKind::Tag,
             )))
+        );
+    }
+
+    #[test]
+    fn whitespace_characters() {
+        assert_eq!(whitespace(&b"    {"[..]), Ok((&b"{"[..], &b"    "[..])));
+        assert_eq!(
+            whitespace(&b"\nabcdef"[..]),
+            Ok((&b"abcdef"[..], &b"\n"[..])),
+        );
+        assert_eq!(
+            whitespace(&b"\t  \r\nabcdef"[..]),
+            Ok((&b"abcdef"[..], &b"\t  \r\n"[..])),
+        );
+
+        let input = &b"\r\n\r\n  {\"field\": \"abcdef\"}\r\n\r\n"[..];
+        assert_eq!(
+            whitespace(input),
+            Ok((&b"{\"field\": \"abcdef\"}\r\n\r\n"[..], &b"\r\n\r\n  "[..]))
         );
     }
 }
