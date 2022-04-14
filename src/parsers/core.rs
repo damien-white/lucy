b967fc28a005b2e0dba08bb77f2279c067bb76fc
Peter Donovan <peter@darkfusion.io>
2022-04-14 03:39:53 -0400

    Parsers now take `&str` slices to handle unicode
    Lucy's parser functions initially only took raw byte slices as inputs.
    This is not the correct approach to take as it does not factor in the
    idea of unicode code points. `char` types can be anywhere from 1-4 bytes
    in length and failing to perform proper validation results in undefined
    behavior.

--- src/parsers/core.rs
+++ src/parsers/core.rs
@@ -4,8 +4,8 @@ use std::collections::HashMap;
 
 use nom::branch::alt;
 use nom::bytes::complete::{escaped, tag, take_while, take_while1};
-use nom::character::complete::{char, one_of};
-use nom::combinator::{cut, map, map_res, value};
+use nom::character::complete::{char as token, one_of};
+use nom::combinator::{cut, map, value};
 use nom::multi::separated_list0;
 use nom::number::complete::double;
 use nom::sequence::{preceded, separated_pair, terminated};
@@ -14,69 +14,66 @@ use nom::IResult;
 use crate::types::Value;
 
 /// Parses a JSON value with the `array` type.
-pub fn array(input: &[u8]) -> IResult<&[u8], Vec<Value>> {
+pub fn array(input: &str) -> IResult<&str, Vec<Value>> {
     preceded(
-        char('['),
+        token('['),
         cut(terminated(
-            separated_list0(preceded(whitespace, char(',')), json_value),
-            preceded(whitespace, char(']')),
+            separated_list0(preceded(whitespace, token(',')), json_value),
+            preceded(whitespace, token(']')),
         )),
     )(input)
 }
 
 /// Parses a JSON value with the `boolean` type.
-pub fn boolean(input: &[u8]) -> IResult<&[u8], bool> {
-    alt((value(true, tag(b"true")), value(false, tag(b"false"))))(input)
+pub fn boolean(input: &str) -> IResult<&str, bool> {
+    alt((value(true, tag("true")), value(false, tag("false"))))(input)
 }
 
 /// Parses a JSON value with the `object` type.
-pub fn object(input: &[u8]) -> IResult<&[u8], HashMap<&str, Value>> {
+pub fn object(input: &str) -> IResult<&str, HashMap<&str, Value>> {
     preceded(
-        char('{'),
+        token('{'),
         cut(terminated(
             map(
-                separated_list0(preceded(whitespace, char(',')), key_value_pair),
+                separated_list0(preceded(whitespace, token(',')), key_value_pair),
                 |entries| collect_entries(&entries),
             ),
-            preceded(whitespace, char('}')),
+            preceded(whitespace, token('}')),
         )),
     )(input)
 }
 
 /// Parses a JSON value with the `null` type.
-pub fn null(input: &[u8]) -> IResult<&[u8], ()> {
+pub fn null(input: &str) -> IResult<&str, ()> {
     value((), tag("null"))(input)
 }
 
 /// Parses a JSON value with the `number` type.
-pub fn number(input: &[u8]) -> IResult<&[u8], f64> {
+pub fn number(input: &str) -> IResult<&str, f64> {
     double(input)
 }
 
 /// Parses a JSON value with the `string` type.
-pub fn string(input: &[u8]) -> IResult<&[u8], &str> {
+pub fn string(input: &str) -> IResult<&str, &str> {
     preceded(
-        char('\"'),
+        token('\"'),
         cut(terminated(
-            map_res(
-                escaped(take_while1(is_string_char), '\\', one_of("\"bfnrt\\")),
-                std::str::from_utf8,
-            ),
-            char('\"'),
+            escaped(take_while1(is_string_token), '\\', one_of("\"bfnrt\\")),
+            token('\"'),
         )),
     )(input)
 }
 
 // ===== Utility functions and miscellaneous parsers =====
-pub(crate) fn key_value_pair(input: &[u8]) -> IResult<&[u8], (&str, Value)> {
+pub(crate) fn key_value_pair(input: &str) -> IResult<&str, (&str, Value)> {
     separated_pair(
         preceded(whitespace, string),
-        cut(preceded(whitespace, char(':'))),
+        cut(preceded(whitespace, token(':'))),
         json_value,
     )(input)
 }
 
-pub fn json_value(input: &[u8]) -> IResult<&[u8], Value> {
+pub fn json_value(input: &str) -> IResult<&str, Value> {
     preceded(
         whitespace,
         alt((
@@ -95,18 +92,18 @@ fn collect_entries<'a>(entries: &[(&'a str, Value<'a>)]) -> HashMap<&'a str, Val
     HashMap::from_iter(entries.iter().cloned())
 }
 
-fn whitespace(input: &[u8]) -> IResult<&[u8], &[u8]> {
+fn whitespace(input: &str) -> IResult<&str, &str> {
     take_while(is_whitespace)(input)
 }
 
 #[inline]
-fn is_string_char(c: u8) -> bool {
-    c != b'"' && c != b'\\'
+fn is_string_token(c: char) -> bool {
+    c != '"' && c != '\\'
 }
 
 #[inline]
-fn is_whitespace(ch: u8) -> bool {
-    ch == b' ' || ch == b'\t' || ch == b'\r' || ch == b'\n'
+fn is_whitespace(ch: char) -> bool {
+    ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n'
 }
 
 #[cfg(test)]
@@ -119,9 +116,9 @@ mod tests {
     #[test]
     fn array_values() {
         assert_eq!(
-            array(&b"[5, 42, 97.5]abcd"[..]),
+            array("[5, 42, 97.5]abcd"),
             Ok((
-                &b"abcd"[..],
+                "abcd",
                 vec![
                     Value::Number(5_f64),
                     Value::Number(42_f64),
@@ -130,9 +127,9 @@ mod tests {
             ))
         );
         assert_eq!(
-            array(&b"[true, \"lucy\", null]abcd"[..]),
+            array("[true, \"lucy\", null]abcd"),
             Ok((
-                &b"abcd"[..],
+                "abcd",
                 vec![Value::Boolean(true), Value::String("lucy"), Value::Null,]
             ))
         );
@@ -140,87 +137,65 @@ mod tests {
 
     #[test]
     fn boolean_values() {
-        assert_eq!(boolean(&b"true\"\nxxx"[..]), Ok((&b"\"\nxxx"[..], true)));
-        assert_eq!(boolean(&b"falsexyz"[..]), Ok((&b"xyz"[..], false)));
+        assert_eq!(boolean("true\"\nxxx"), Ok(("\"\nxxx", true)));
+        assert_eq!(boolean("falsexyz"), Ok(("xyz", false)));
         assert_eq!(
-            boolean(&b"abcdef"[..]),
-            Err(Err::Error(Error::new(&b"abcdef"[..], ErrorKind::Tag)))
+            boolean("abcdef"),
+            Err(Err::Error(Error::new("abcdef", ErrorKind::Tag)))
         );
         assert_eq!(
-            boolean(&b"xyzfalse"[..]),
-            Err(Err::Error(Error::new(&b"xyzfalse"[..], ErrorKind::Tag)))
+            boolean("xyzfalse"),
+            Err(Err::Error(Error::new("xyzfalse", ErrorKind::Tag)))
         );
-        assert_eq!(
-            boolean(&b""[..]),
-            Err(Err::Error(Error::new(&b""[..], ErrorKind::Tag)))
-        );
+        assert_eq!(boolean(""), Err(Err::Error(Error::new("", ErrorKind::Tag))));
     }
 
     #[test]
     fn nullish_values() {
-        assert_eq!(null(&b"nullabcd"[..]), Ok((&b"abcd"[..], ())));
+        assert_eq!(null("nullabcd"), Ok(("abcd", ())));
         assert_eq!(
-            null(&b"abcdef"[..]),
-            Err(Err::Error(Error::new(&b"abcdef"[..], ErrorKind::Tag)))
+            null("abcdef"),
+            Err(Err::Error(Error::new("abcdef", ErrorKind::Tag)))
         );
         assert_eq!(
-            null(&b"abcdnull"[..]),
-            Err(Err::Error(Error::new(&b"abcdnull"[..], ErrorKind::Tag)))
+            null("abcdnull"),
+            Err(Err::Error(Error::new("abcdnull", ErrorKind::Tag)))
         );
     }
 
     #[test]
     fn number_values() {
         // let input = &"8".as_bytes_mut();
-        let input = &br#"8"#[..];
+        let input = r#"8"#;
         let expected = 8.;
-        assert_eq!(number(input), Ok((&b""[..], expected)));
+        assert_eq!(number(input), Ok(("", expected)));
 
-        assert_eq!(number(&b"-1.234E-12"[..]), Ok((&b""[..], -1.234e-12)))
+        assert_eq!(number("-1.234E-12"), Ok(("", -1.234e-12)))
     }
 
     #[test]
     fn string_values() {
+        assert_eq!(string("\"0123456789abcdef\""), Ok(("", "0123456789abcdef")));
         assert_eq!(
-            string(r#""0123456789abcdef""#.as_bytes()),
-            Ok((&b""[..], "0123456789abcdef"))
+            string("\"0x00 0x01 0x04 0xDE 0xAD\""),
+            Ok(("", "0x00 0x01 0x04 0xDE 0xAD"))
         );
         assert_eq!(
-            string(&br#""0x00 0x01 0x04 0xDE 0xAD""#[..]),
-            Ok((&b""[..], "0x00 0x01 0x04 0xDE 0xAD"))
-        );
-        assert_eq!(
-            string(&br#""'0x00 0x01 0x04 0xDE 0xAD'""#[..]),
-            Ok((&b""[..], "'0x00 0x01 0x04 0xDE 0xAD'"))
+            string("\"'0x00 0x01 0x04 0xDE 0xAD'\""),
+            Ok(("", "'0x00 0x01 0x04 0xDE 0xAD'"))
         );
     }
 
-    // #[test]
-    // fn unicode_values_in_strings() {
-    //     assert_eq!(string(&br#""a\\u005Cb"XYZ"#[..]), Ok((&b"XYZ"[..], "a\\b")));
-    //     let data = "\"tab:\\tafter tab, newline:\\nnew line, quote: \\\", emoji: \\u{1F602}, newline:\\nescaped whitespace: \\    abc\"";
-    //     println!(
-    //         "EXAMPLE 2:\nParsing a string with escape sequences, newline literal, and escaped whitespace:\n\n{}\n",
-    //         data
-    //     );
-    // }
-
     #[test]
     fn whitespace_characters() {
-        assert_eq!(whitespace(&b"    {"[..]), Ok((&b"{"[..], &b"    "[..])));
-        assert_eq!(
-            whitespace(&b"\nabcdef"[..]),
-            Ok((&b"abcdef"[..], &b"\n"[..])),
-        );
-        assert_eq!(
-            whitespace(&b"\t  \r\nabcdef"[..]),
-            Ok((&b"abcdef"[..], &b"\t  \r\n"[..])),
-        );
+        assert_eq!(whitespace("    {"), Ok(("{", "    ")));
+        assert_eq!(whitespace("\nabcdef"), Ok(("abcdef", "\n")),);
+        assert_eq!(whitespace("\t  \r\nabcdef"), Ok(("abcdef", "\t  \r\n")),);
 
-        let input = &b"\r\n\r\n  {\"field\": \"abcdef\"}\r\n\r\n"[..];
+        let input = "\r\n\r\n  {\"field\": \"abcdef\"}\r\n\r\n";
         assert_eq!(
             whitespace(input),
-            Ok((&b"{\"field\": \"abcdef\"}\r\n\r\n"[..], &b"\r\n\r\n  "[..]))
+            Ok(("{\"field\": \"abcdef\"}\r\n\r\n", "\r\n\r\n  "))
         );
     }
 }
--- src/parsers/unicode.rs
+++ src/parsers/unicode.rs
@@ -1,1 +1,33 @@
+use nom::bytes::complete::take_while_m_n;
+use nom::character::complete::char as token;
+use nom::combinator::map_res;
+use nom::sequence::{delimited, preceded};
+use nom::IResult;
 
+// TODO: Flesh POC parser out so it can handle escaped unicode sequences
+//       Escaped sequences will help produce actual unicode characters
+pub fn unicode_sequence(input: &str) -> IResult<&str, u32> {
+    let hex_digits = take_while_m_n(1, 6, |ch: char| ch.is_ascii_hexdigit());
+    map_res(
+        preceded(token('u'), delimited(token('{'), hex_digits, token('}'))),
+        |val| u32::from_str_radix(val, 16),
+    )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use nom::error::{Error, ErrorKind};
+    use nom::Err;
+
+    use super::*;
+
+    #[test]
+    fn unicode_values_in_strings() {
+        assert_eq!(unicode_sequence("u{005C}XYZ"), Ok(("XYZ", 92)));
+        assert_eq!(unicode_sequence("u{7F80}"), Ok(("", 32640)));
+        assert_eq!(
+            unicode_sequence("a\\b"),
+            Err(Err::Error(Error::new("a\\b", ErrorKind::Char)))
+        );
+    }
+}
